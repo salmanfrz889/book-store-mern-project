c6a56af71ab8c43f922b2c41955116621127d9b2
salmanul faris <salmanfrz889@gmail.com>
2024-11-20 14:12:14 +0530

    updated

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <nav>Navbar</nav>
+     <main className='h-screen max-w-screen-2xl mx-auto px-4 py-6'>
+      <Outlet/>
+     </main>
+     <footer>Footer</footer>
+    </>
+  )
+}
+
+export default App
